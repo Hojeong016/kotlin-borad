dbf5ff580f3971c04304170d77c4c96622b623a0
Hogeong016 <skflsehdn@naver.com>
2024-10-20 23:01:44 +0900

    ADD ExceptionHandler

---
+++ src/main/kotlin/com/example/demo/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,17 @@
+package com.example.demo.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(NullPointerException::class)
+    fun nullpointerExceptionHandler(er : NullPointerException) : ResponseEntity<String> = ResponseEntity<String>("null값이 들어왔습니다.",HttpStatus.BAD_REQUEST)
+
+
+    @ExceptionHandler(Exception::class)
+    fun AllException(err: Exception) : ResponseEntity<String> = ResponseEntity<String>("서버 오류가 발생했습니다.",HttpStatus.INTERNAL_SERVER_ERROR)
+}
